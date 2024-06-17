aa97c89a7d2754a81e0d9bc26f1344a380e00706
Jueuunn7 <wndmsrla1234@gmail.com>
2024-06-17 16:00:07 +0900

    Refector

--- common/views.py
+++ common/views.py
@@ -17,17 +17,17 @@ class ImageView(APIView):
     def post(self, request: object) -> Response:
         image = request.FILES.get('image')
         if not image:
-            return Response({'error':'Image not found'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Image not found'}, status=status.HTTP_400_BAD_REQUEST)
 
         name, extension = os.path.splitext(image.name)
         if extension.lower() not in self.valid_image_extension:
-            return Response({'msg':'Invalid extension'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'msg': 'Invalid extension'}, status=status.HTTP_400_BAD_REQUEST)
 
         if image.content_type not in self.valid_image_content:
-            return Response({'msg':'Invalid image'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'msg': 'Invalid image'}, status=status.HTTP_400_BAD_REQUEST)
 
         if image.size > 5 * 1024 * 1024:
-            return Response({'msg':'Image too large'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'msg': 'Image too large'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
             path = f'{uuid.uuid4()}{extension}'
@@ -36,6 +36,6 @@ class ImageView(APIView):
             s3path = f'https://{AWS_S3_INSTANCE_URL}/{save}'
             Image.objects.create(url=s3path)
         except Exception as e:
-            return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({'url': s3path}, status=status.HTTP_200_OK)
